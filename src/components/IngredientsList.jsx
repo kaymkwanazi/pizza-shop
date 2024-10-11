796077cfa1a0ef36a2785838e0a06b7bb63a58f2
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-10-11 12:04:54 +0200

    list

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export const IngredientsList = () => {
+  const location = useLocation();
+  const {item} = location.state;
+
   return (
-    <div>
+    <div className='container mx-auto px-4 mt-5'>
+      <h1 className='text-4xl font-bold mb-5'>{item.name}</h1>
+      <h2 className='text-2xl font-semibold mb-3'>Ingredients</h2>
+      <ul>
+        {item.ingredients.map((ingredients, index) => (
+          <li key={index}>{ingredients}</li>
+        ))}
+      </ul>
         
     </div>
   )
--- src/components/MenuComp.jsx
+++ src/components/MenuComp.jsx
@@ -11,15 +11,17 @@ export const MenuComp = () => {
   
   return (
     <div className="grid cols-1 gap-4 mt-80">
-      <div className='container mx-auto px-4 border-y mb-36'>
+      <div className='container mx-auto px-5 bg-slate-100 border-2 mb-36'>
       {Menu[0].menu.map((products, index) => (
         <div key={index} className="mb-8 p-4">
           <h2 className="text-4xl md:text-6xl text-blue-700 font-bold mb-4">{products.category}</h2>
           <p className="text-xl mb-20">{products.description}</p>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-20">
             {products.items.map((item) => (
-              <div key={item.id} className="flex flex-col cursor-pointer" onClick={() => handleItemClick(item)}>
-                <img src={item.imageUrl} alt={item.name} className="w-52 h-52 object-cover mb-4 transform transition duration-300 hover:scale-110" />
+              <div key={item.id} className="flex flex-col" >
+                <img src={item.imageUrl} alt={item.name} className="w-52 h-52 object-cover cursor-pointer mb-4 transform transition duration-300 hover:scale-110" 
+                  onClick={() => handleItemClick(item)}
+                />
                 <h1 className="font-bold text-xl md:text-2xl mb-3">{item.name}</h1>
                 <p className="mb-5 ">{item.description}</p>
               </div>
